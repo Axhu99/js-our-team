207081c42e26995f026eccc3e42c5536f5de681a
Axhu99 <xhuliodreni@icloud.com>
2024-01-15 16:30:10 +0100

    add milestone 0

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//MILESTONE 0 creo il mio array
+const society = [
+    /* troviamo il nome, la posizone occupata e immagine profilo di una determinata persona */
+    {   
+        name: 'Wayne Barnett',
+        position: 'Founder & CEO',
+        img:'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name: 'Angela Caroll',
+        position: 'Chief Editor ',
+        img:'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name: 'Walter Gordon',
+        position: 'Office Manager',
+        img:'walter-gordon-office-manager.jpg',
+    },
+    {
+        name: 'Angela Lopez',
+        position: 'Social Media Manager',
+        img:'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name: 'Scott Estrada',
+        position: 'Developer',
+        img:'scott-estrada-developer.jpg',
+    },
+    {
+        name: 'Barbara Ramos',
+        position: 'Graphic Designer',
+        img:'barbara-ramos-graphic-designer.jpg',
+    }
+    
+]
